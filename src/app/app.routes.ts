66cf70af4af721fa11c8af9c0536d4e24e2f6b93
JoseJ <alvarezreyes246@gmail.com>
2025-05-21 18:52:00 -0500

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeTemplateComponent } from './home-template/home-template.component';
+import { InitialContentComponent } from './pages/initial-content/initial-content.component';
+import { ContentComponent } from './pages/content/content.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeTemplateComponent,
+    children: [
+      { path: '', component: InitialContentComponent },
+      { path: 'last', component: ContentComponent },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
