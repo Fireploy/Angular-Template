124cc9114e868e4aa22b19e5b929b03b161094a5
Fireploy <fireployvm@gmail.com>
2025-05-28 20:25:41 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,17 +14,5 @@ import { DOCUMENT } from '@angular/common'; // Usamos @angular/common
 })
 export class AppComponent {
   constructor(@Inject(DOCUMENT) private document: Document) {}
-
-  ngOnInit(): void {
-    // Accedemos a todos los elementos <base> en el documento
-    const bases = this.document.getElementsByTagName('base');
-
-    // Verificamos que haya al menos un elemento <base>
-    if (bases.length > 0) {
-      // Establecemos dinámicamente el atributo href de la primera etiqueta <base>
-      bases[0].setAttribute('href', environment.basePath);
-    }
-  }
-
   title = 'template-Angular';
 }
